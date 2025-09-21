9ea57f1de20e97b0022ebe6699a4c11c6998f830
Ariful Islam <arifulq234@gmail.com>
2025-09-22 04:05:05 +0600

    implimented toggole features

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,37 @@ const [selectToggole, SetToggole]=useState(true)
   return (
     <>
       <Navbar></Navbar>
-
-      <Suspense 
-        fallback={<span className="loading loading-spinner text-error text-center "></span>}
-      >
-        <AvailablePlayer  playerPromise={playerPromise}></AvailablePlayer>
-      </Suspense>
-      <SlectedPlayer></SlectedPlayer>
-      
+      {/* Availabe button and slect on availabe toggole here */}
+      <div className='flex justify-between items-center max-w-[1280px] mx-auto'>
+        <h1 className='font-extrabold text-2xl'>Available Players</h1>
+        <div>
+          <button onClick={()=> SetToggole(true)}
+            className={`py-3 px-4 font-bold border-1 border-gray-300 rounded-l-2xl border-r-0 ${
+              selectToggole === true ? "bg-[#E7FE29]" : ""
+            }`}
+          >
+            Aailabe
+          </button>
+          <button onClick={()=> SetToggole(false)}
+            className={`py-3 px-4 font-bold border-1 border-gray-300 rounded-r-2xl border-l-0  ${
+              selectToggole === false ? "bg-[#E7FE29]" : ""
+            } `}
+          >
+            Selected
+          </button>
+        </div>
+      </div>
+      {selectToggole ? (
+        <Suspense
+          fallback={
+            <span className="loading loading-spinner text-error text-center "></span>
+          }
+        >
+          <AvailablePlayer playerPromise={playerPromise}></AvailablePlayer>
+        </Suspense>
+      ) : (
+        <SlectedPlayer></SlectedPlayer>
+      )}
     </>
   );
 }
--- src/Components/SelectedPlayer/SlectedPlayer.jsx
+++ src/Components/SelectedPlayer/SlectedPlayer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SlectedPlayer = () => {
     return (
-        <div className='max-w-[1280[px] mx-auto'>
+        <div className='max-w-[1280px] mx-auto'>
           <h1>its selected Part</h1>  
         </div>
     );
